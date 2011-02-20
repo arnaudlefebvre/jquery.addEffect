b908d5ab362a2c7fba06c6de87ca0423b0557db0
Arnaud <arnd.lefebvre@gmail.com>
2011-02-20 19:11:29 +0100

    Update commentaries

--- jquery.addeffect.js
+++ jquery.addeffect.js
@@ -3,73 +3,14 @@
 /* 																																*
 /* This script allow you add JQuery effects on elements defining																*
 /* one or more configuration. 																									*
-/* These configurations have to be set with 7 elements :																		*
-/* - cvTitleEltClass //Class of elements which is the title (not really used now but required)									*
-/* - cvInfosEltClass //Class of elements which contains more informations														*
+/* These configurations have to be set with 5 elements :																		*														*
 /* - cvMoreEltClass  //Class of elements on which a click show cvInfosEltClass													*
 /* - cvLessEltClass  //Class of elements on which a click hide cvInfosEltClass													*
 /* - effect			 //Effect which be apply when displaying or hidding cvInfosEltClass											*
 /* - time			 //Duration in millisecond of the effect when displaying cvInfosEltClass									*
 /* - hiddingTime (optionnal) //Duration in millisecond of the effect when hidding cvInfosEltClass								*
 /*																																*
-/********************************************************************************************************************************
-/*
-/* Here is an example of how to use the script :
-/*
-/* <html>
-/* 	<head>
-/* 		<script src="js/jquery.js"></script>
-/*		<style>
-/*			.cvInfosEltClass1 { float:right;}
-/*			.cvInfosEltClass1 { float:left;}
-/*		</style>
-/* 	</head>
-/* 	<body>
-/* 		<table>
-/* 			<tr>
-/* 				<td><div id="cvwhat" class="cvwhat">This a very good movie.</div></td>
-/* 				<td><span class="cvMoreEltClass">More infos ?</span>
-/* 				<td><span class="cvLessEltClass">Less infos ?</span></td>
-/*			</tr>
-/* 		</table>
-/*		<div class="cvInfosEltClass">
-/*			This bloc contains more information
-/*			about this very good movie.
-/*		</div>
-/*
-/*		<table>
-/* 			<tr>
-/* 				<td><div id="cvwhat1" class="cvwhat1">This a very good movie.</div></td>
-/* 				<td><span class="cvMoreEltClass1">More infos ?</span>
-/* 				<td><span class="cvLessEltClass1">Less infos ?</span></td>
-/*			</tr>
-/* 		</table>
-/*	<div class="cvInfosEltClass1">
-/*		This bloc contains more information
-/*		about this very good movie.
-/*	</div>
-/*
-/*	<br />
-/*	<span id="cvwhat2" class="cvwhat2">This a very good movie.</span>
-/*	<span class="cvMoreEltClass2">More infos ?</span>
-/*	<span class="cvLessEltClass2">Less infos ?</span>
-/*	<span class="cvInfosEltClass2">
-/*		This bloc contains more information
-/*		about this very good movie.
-/*	</span>
-/*
-/* 	</body>
-/*	<script src="js/jquery.js"></script>
-/*      <script src="js/jquery.cvscriptplugin.js"></script>
-/* 	<script type="text/javascript">
-/*
-/* 	$("#cvwhat").hideOrShowClassPlugin({cvMoreEltClass:'.cvMoreEltClass',cvLessEltClass:'.cvLessEltClass',cvInfosEltClass:'.cvInfosEltClass'});
-/* 	$("#cvwhat1").hideOrShowClassPlugin({cvMoreEltClass:'.cvMoreEltClass1',cvLessEltClass:'.cvLessEltClass1',cvInfosEltClass:'.cvInfosEltClass1'});
-/* 	$("#cvwhat2").hideOrShowClassPlugin({cvMoreEltClass:'.cvMoreEltClass2',cvLessEltClass:'.cvLessEltClass2',cvInfosEltClass:'.cvInfosEltClass2'});
-/*
-/*      </script>
-/* </html>
-/****************************************************************************************************/
+/********************************************************************************************************************************/
 function Constants() {
     this.Constants = Constants.EFFECTS.none;
 }
@@ -126,7 +67,7 @@ Constants.GLOBAL = {
             }
         }
 
-        // définition des paramètres par défaut
+        // definition des parametres par defaut
         /****************************************
          *Variable par defauts :
          *cvMoreEltClass : ".cvMoreEltClass",
@@ -145,10 +86,10 @@ Constants.GLOBAL = {
             hiddingTime : Constants.EFFECTS.hiddingTime,
             cvTitleEltClass : ".cvTitleEltClass"
         }
-        // mélange des paramètres fournis et des paramètres par défaut
+        // melange des parametres fournis et des parametres par defaut
         var opts = $.extend(defaults, options);
         
-        /*Définitions des var */
+        /*Definitions des var */
         config0 = new Configuration();
         config0.cvMoreEltClass = opts.cvMoreEltClass;
         config0.cvLessEltClass = opts.cvLessEltClass;
@@ -166,7 +107,7 @@ Constants.GLOBAL = {
         $(Constants.CONF.current[Constants.GLOBAL.nbconf].cvLessEltClass).hide();
         $(this).slideUp(1);
         
-        /*Pour chacun des elts $(this) on ajout les effets sur les élements qui le concerne */
+        /*Pour chacun des elts $(this) on ajout les effets sur les elements qui le concerne */
          $(this).each(function(index, infosElement) {
             var idx = Constants.CONF.current[Constants.GLOBAL.nbconf].idx;
             Constants.CONF.current[idx].cvInfosEltClass = infosElement;
@@ -241,23 +182,6 @@ Constants.GLOBAL = {
             }
          })
 
-         //Regexp pour .animate
-         //\{(.)+},|\s*(['a-zA-Z-0-9()]+),|\S*(function\(\)(\s)*\{(.*)\})++
-
-        //var hideOrShowClass = new HideOrShowClass();
-
-        /**AJOUT D'UNE CONFIGURATION**/
-        /*hideOrShowClass.cvMoreEltClass = opts.cvMoreEltClass;
-        hideOrShowClass.cvLessEltClass = opts.cvLessEltClass;
-        hideOrShowClass.cvInfosEltClass = opts.cvInfosEltClass;
-        hideOrShowClass.effect = opts.effect;
-        hideOrShowClass.cvTime = opts.cvTime;
-        /** FIN AJOUT D'UNE CONFIGURATION **/
-
-        /*hideOrShowClass.setUp();
-
-        hideOrShowClass.addEffects();
-*/
         Constants.GLOBAL.nbconf++;
         return $(this);
     };
