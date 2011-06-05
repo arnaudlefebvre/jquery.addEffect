ecc3d3162372f09397c25a327387d8d0adfc1776
Arnaud Lefebvre <git@arnaud-lefebvre.fr>
2011-06-05 16:38:52 +0200

    Add multi-click elements handler, modication of parameters allowed. Add errors on effect detection

--- jquery.addeffect.js
+++ jquery.addeffect.js
@@ -1,188 +1,245 @@
-/********************************************************************************************************************************
-/* addEffect.js - 07/02/2011 - Arnaud Lefebvre - Using JQuery																*
-/* 																																*
-/* This script allow you add JQuery effects on elements defining																*
-/* one or more configurations. 																									*
-/* These configurations have to be set with 5 elements :																		*														*
-/* - cvMoreEltClass  //Class of elements on which a click show cvInfosEltClass													*
-/* - cvLessEltClass  //Class of elements on which a click hide cvInfosEltClass													*
-/* - effect			 //Effect which be applied when displaying or hidding cvInfosEltClass											*
-/* - time			 //Duration in millisecond of the effect when displaying cvInfosEltClass									*
-/* - hiddingTime (optionnal) //Duration in millisecond of the effect when hidding cvInfosEltClass								*
-/*																																*
-/********************************************************************************************************************************/
-
-function Constants() {
-    this.Constants = Constants.EFFECTS.none;
-}
-
-Constants.EFFECTS = {
-    none : -1,
-    slide : 0,
-    fade: 1,
-    toggle : 2,
-    cvTime : "800",
-    hiddingTime : "300"
-}
-
-Constants.ELEMENTS = {
-    title : "title",
-    more : "more",
-    less : "less",
-    infos : "infos",
-    clmore : "clmore",
-    cltitle : "cltitle",
-    clless : "clless",
-    clinfos : "clinfos"
-}
-
-Constants.CONF = {
-    current : Array()
-}
-
-Constants.GLOBAL = {
-    nbElementsMax : 100,
-    identifier : "_",
-    clend : "AZBYCX",
-    nbconf : 0
-};
+/************************************************************************************************************************************/
+/* addEffect.js - 07/02/2011 - Arnaud Lefebvre - Using JQuery												*
+/* 																						*
+/* This script allow you add JQuery effects on elements defining												*
+/* one or more configurations.                                                                                                      *
+/* Let info the elements on which the plugin is applied 													*
+/* These configurations have to be set with 5 elements :													*														*
+/* - more  //Class of elements on which a click show info													*
+/* - less  //Class of elements on which a click hide info													*
+/* - effect			 //Effect which be applied when displaying or hidding info                                                  *
+/* - time			 //Duration in millisecond of the effect when displaying info                                               *
+/* - hiddingTime (optionnal) //Duration in millisecond of the effect when hidding info                                              *
+/*                                                                                                                                  *
+/************************************************************************************************************************************/
 
 (function($) {
     $.fn.addEffect = function(options) {
 
+        /**
+         *Constants
+         *
+         **/
+        function Constants() {
+            this.Constants = Constants.EFFECTS.none;
+        }
+
+        Constants.EFFECT_TAB = {
+            effect : new Array(
+                new Array("slideUp","slideDown"),
+                new Array("fadeIn","fadeOut"),
+                new Array("toogle","toogle"),
+                new Array("hide","show")
+            )
+        }
+        Constants.EFFECT_TAB.effect["slide"] = new Array("slideUp","slideDown");
+        Constants.EFFECT_TAB.effect["fade"] = new Array("fadeOut","fadeIn");
+        Constants.EFFECT_TAB.effect["toogle"] = new Array("toogle","toogle");
+        Constants.EFFECT_TAB.effect["hide"] = new Array("hide","show");
+        Constants.EFFECT_TAB.effect["show"] = new Array("hide","show");
+
+        Constants.EFFECTS = {
+            none : 3,
+            slide : 0,
+            fade: 1,
+            toggle : 2,
+            time : "800",
+            hiddingTime : "300"
+        }
+
+        Constants.CONF = {
+            current : Array()
+        }
+
+        Constants.GLOBAL = {
+            nbconf : 0
+        };
+
+        function getNumberOfMoreElements(more,affected) {
+            var error = $(more).length() % $(affected).length();
+            if (error != 0) {
+                throw new Error("Error : There is missing HTML tags or bad configuration.");
+            }
+            else {
+                return $(more).length() / $(affected).length();
+            }
+        }
+
+        function getNumberOfLessElements(less,affected) {
+            var error = $(less).length() % $(affected).length();
+            if (error != 0) {
+                throw new Error("Error : There is missing HTML tags or bad configuration.");
+            }
+            else {
+                return $(less).length() / $(affected).length();
+            }
+        }
+
+        /**
+         *Effect class
+         *
+         *
+         **/
+        function Effect() {
+            this.up = Constants.EFFECTS.none;
+            this.down = Constants.EFFECTS.none;
+
+             if( typeof Effect.initialized == "undefined" ) {
+                /**toString() function
+                 */
+                Effect.prototype.toString = function() {
+                    return "effectDown : "+this.down+"\neffectUp : "+this.up;
+                };
+
+                /**Convert any var which is number into Number
+                 * @return number
+                 * @throws Exception
+                 **/
+                Effect.prototype.toNumber = function(effect) {
+                    if (isNaN(effect*1)==false)
+                        effect = effect*1
+                    return effect;
+                }
+
+                /**Set the effect properties.
+                 *
+                 * @param effect, String or number, defines which effects will be applied for
+                 *              showing or hidding elements.
+                 **/
+                Effect.prototype.setEffect = function(effect) {
+                    effect = this.toNumber(effect);
+                    if (typeof effect == 'number' && effect < Constants.EFFECT_TAB.effect.length ) {
+                        this.up = Constants.EFFECT_TAB.effect[effect][0];
+                        this.down = Constants.EFFECT_TAB.effect[effect][1];
+                    }
+                    else if (typeof effect == 'string') {
+                        try {
+                            this.up = Constants.EFFECT_TAB.effect[effect.toLowerCase()][0];
+                            this.down = Constants.EFFECT_TAB.effect[effect.toLowerCase()][1];
+                        } catch (e) {
+                            throw new Error("Specified effect is unknown (effect:"+effect.toLowerCase()+"). Please check configuration.");
+                        }
+                    }
+                    else
+                        throw new Error("Specified effect is unknown (effect:"+effect+"). Please check configuration.");
+                };
+
+
+             Effect.initialized = true;
+             }
+        }
+
+        /**Configuration class
+         * Class in which we save elements and effect to apply.
+         * On each call of jquery.addEffect one Configuration is
+         * instaciated.
+         *
+         *
+         **/
         function Configuration() {
-            this.cvMoreEltClass = "";
-            this.cvLessEltClass = "";
-            this.cvInfosEltClass = "";
-            this.effect = Constants.EFFECTS.none;
+            this.more = "";
+            this.nbMore = 1;
+            this.less = "";
+            this.nbLess = 1;
+            this.info = "";
+            this.effect = new Effect();
             this.time = Constants.EFFECTS.none;
             this.hiddingTime = Constants.EFFECTS.hiddingTime;
-            this.idx = 0;
             this.current = null;
+            this.idx = 0;
 
             if( typeof Configuration.initialized == "undefined" ) {
                  //toString() function
                 Configuration.prototype.toString = function() {
-                    return "Configuration : \ncvMoreEltClass = "+this.cvMoreEltClass+",\ncvLessEltClass = "+this.cvLessEltClass+",\ncvInfosEltClass = "+this.cvInfosEltClass+",\neffect = "+this.effect+",\ncvtime = "+this.time;
+                    return "Configuration : \nmore = "+this.more+
+                        ",\nbMore = "+this.nbMore+
+                        ",\nless = "+this.less+
+                        ",\nbLess = "+this.nbLess+
+                        ",\ninfo = "+this.info+
+                        ",\neffectUp = "+this.effectUp+
+                        ",\neffectDown = "+this.effectDown+
+                        ",\ntime = "+this.time;
                 };
+
+                Configuration.prototype.setEffect = function(effect) {
+                    this.effect.setEffect(effect);
+                };
+
                 Configuration.initialized = true;
             }
         }
 
-        // Definition of defaults variables
         /****************************************
          *Defaults vars :
-         *cvMoreEltClass : ".cvMoreEltClass",
-         *cvLessEltClass : ".cvLessEltClass",
-         *cvInfosEltClass : "cvInfosEltClass",
+         *more : ".more",
+         *less : ".less",
+         *info : "info",
          *effect : Constants.EFFECTS.toggle,
-         *time : "-1",
-         *hiddingTime : "-1"
+         *time : "800",
+         *hiddingTime : "300"
          *****************************************/
         var defaults = {
-            cvMoreEltClass : ".cvMoreEltClass",
-            cvLessEltClass : ".cvLessEltClass",
-            cvInfosEltClass : ".cvInfosEltClass",
+            more : ".more",
+            less : ".less",
+            info : ".info",
             effect : Constants.EFFECTS.toggle,
-            time : Constants.EFFECTS.cvTime,
+            time : Constants.EFFECTS.time,
             hiddingTime : Constants.EFFECTS.hiddingTime,
-            cvTitleEltClass : ".cvTitleEltClass"
+            nbLess : 1,
+            nbMore : 1
         }
         //Mix defaults vars and user vars
         var opts = $.extend(defaults, options);
 
         /*Define vars for configuration */
         config0 = new Configuration();
-        config0.cvMoreEltClass = opts.cvMoreEltClass;
-        config0.cvLessEltClass = opts.cvLessEltClass;
-        config0.cvInfosEltClass = "."+this[0].className;
-        config0.current = null;
-        config0.effect = opts.effect;
-        config0.time = opts.cvTime;
+        config0.more = opts.more;
+        config0.less = opts.less;
+        config0.info = "."+this[0].className;
+        config0.setEffect(opts.effect);
+        config0.time = opts.time;
         config0.hiddingTime = opts.hiddingTime;
+        config0.nbLess = opts.nbLess;
+        config0.nbMore = opts.nbMore;
         config0.idx = Constants.GLOBAL.nbconf;
         Constants.CONF.current[Constants.GLOBAL.nbconf] = config0;
 
-         /*We hide elements */
-        $(Constants.CONF.current[Constants.GLOBAL.nbconf].cvLessEltClass).hide();
-        $(this).slideUp(1);
+        /*Hide elements */
+        $(Constants.CONF.current[Constants.GLOBAL.nbconf].less).hide();
+        $(this).hide();
 
-        /*For each elements $(this), we add effects on involved elements*/
-         $(this).each(function(index, infosElement) {
-            var idx = Constants.CONF.current[Constants.GLOBAL.nbconf].idx;
-            Constants.CONF.current[idx].current = infosElement;
-            $(Constants.CONF.current[idx].cvMoreEltClass+':eq('+index+')').css('cursor','pointer');
-            if (Constants.CONF.current[idx].effect == Constants.EFFECTS.slide) {
-                $(Constants.CONF.current[idx].cvMoreEltClass+':eq('+index+')').click(function(){
-                    var idx0 = idx;
-                    $(Constants.CONF.current[idx0].cvInfosEltClass+':eq('+index+')').slideDown(Constants.CONF.current[idx0].time,function(){
-                        $(Constants.CONF.current[idx0].cvMoreEltClass+':eq('+index+')').hide();
-                        $(Constants.CONF.current[idx0].cvLessEltClass+':eq('+index+')').show().css('cursor','pointer');
-                    })
-                });
-
-                $(Constants.CONF.current[idx].cvLessEltClass+':eq('+index+')').click(function(){
-                    var idx0 = idx;
-                    $(Constants.CONF.current[idx0].cvInfosEltClass+':eq('+index+')').slideUp(Constants.CONF.current[idx0].time,function(){
-                        $(Constants.CONF.current[idx0].cvLessEltClass+':eq('+index+')').hide();
-                        $(Constants.CONF.current[idx0].cvMoreEltClass+':eq('+index+')').show().css('cursor','pointer');
+        /*For each elements $(this), add effects on involved elements*/
+         $(this).each(function(index) {
+            var currentConfig = Constants.CONF.current[Constants.CONF.current[Constants.GLOBAL.nbconf].idx];
+            for (var itm = 0; itm < currentConfig.nbMore ; itm++) {
+                $(currentConfig.more+':eq('+((index*currentConfig.nbMore)+itm)+')').css('cursor','pointer');
+                $(currentConfig.more+':eq('+((index*currentConfig.nbMore)+itm)+')').click(function(){
+                    $(currentConfig.info+':eq('+index+')')[currentConfig.effect.down](currentConfig.time,function(){
+                        for (var i = 0; i < currentConfig.nbMore; i++) {
+                            $(currentConfig.more+':eq('+((index*currentConfig.nbMore)+i)+')').hide();
+                        }
+                        for (var j = 0; j < currentConfig.nbLess ; j++) {
+                            $(currentConfig.less+':eq('+((index*currentConfig.nbLess)+j)+')').show().css('cursor','pointer');
+                        }
                     })
                 });
             }
-            else if (Constants.CONF.current[idx].effect == Constants.EFFECTS.fade) {
-                $(Constants.CONF.current[idx].cvMoreEltClass+':eq('+index+')').click(function(){
-                    var idx0 = idx;
-                    $(Constants.CONF.current[idx0].cvInfosEltClass+':eq('+index+')').fadeIn(Constants.CONF.current[idx0].time,function(){
-                        $(Constants.CONF.current[idx0].cvMoreEltClass+':eq('+index+')').hide();
-                        $(Constants.CONF.current[idx0].cvLessEltClass+':eq('+index+')').show().css('cursor','pointer');
-                    })
-                });
 
-                $(Constants.CONF.current[idx].cvLessEltClass+':eq('+index+')').click(function(){
-                    var idx0 = idx;
-                    $(Constants.CONF.current[idx0].cvInfosEltClass+':eq('+index+')').fadeOut(Constants.CONF.current[idx0].time,function(){
-                        $(Constants.CONF.current[idx0].cvLessEltClass+':eq('+index+')').hide();
-                        $(Constants.CONF.current[idx0].cvMoreEltClass+':eq('+index+')').show();
-                    })
-                });
-            }
-            else if (Constants.CONF.current[idx].effect == Constants.EFFECTS.toggle) {
-                $(Constants.CONF.current[idx].cvMoreEltClass+':eq('+index+')').click(function(){
-                    var idx0 = idx;
-                    $(Constants.CONF.current[idx0].cvInfosEltClass+':eq('+index+')').toggle(Constants.CONF.current[idx0].time,function(){
-                        $(Constants.CONF.current[idx0].cvMoreEltClass+':eq('+index+')').hide();
-                        $(Constants.CONF.current[idx0].cvLessEltClass+':eq('+index+')').show().css('cursor','pointer');
-                    })
-                });
-
-                $(Constants.CONF.current[idx].cvLessEltClass+':eq('+index+')').click(function(){
-                    var idx0 = idx;
-                    $(Constants.CONF.current[idx0].cvInfosEltClass+':eq('+index+')').toggle(Constants.CONF.current[idx0].time,function(){
-                        $(Constants.CONF.current[idx0].cvLessEltClass+':eq('+index+')').hide();
-                        $(Constants.CONF.current[idx0].cvMoreEltClass+':eq('+index+')').show();
-                    })
-                });
-            }
-            else if (Constants.CONF.current[idx].effect == Constants.EFFECTS.none) {
-                $(Constants.CONF.current[idx].cvMoreEltClass+':eq('+index+')').click(function(){
-                    var idx0 = idx;
-                    $(Constants.CONF.current[idx0].cvInfosEltClass+':eq('+index+')').show(Constants.CONF.current[idx0].time,function(){
-                        $(Constants.CONF.current[idx0].cvMoreEltClass+':eq('+index+')').hide();
-                        $(Constants.CONF.current[idx0].cvLessEltClass+':eq('+index+')').show().css('cursor','pointer');
-                    })
-                });
-
-                $(Constants.CONF.current[idx].cvLessEltClass+':eq('+index+')').click(function(){
-                    var idx0 = idx;
-                    $(Constants.CONF.current[idx0].cvInfosEltClass+':eq('+index+')').hide(Constants.CONF.current[idx0].time,function(){
-                        $(Constants.CONF.current[idx0].cvLessEltClass+':eq('+index+')').hide();
-                        $(Constants.CONF.current[idx0].cvMoreEltClass+':eq('+index+')').show();
+            for (var it = 0; it < currentConfig.nbLess ; it++) {
+                $(currentConfig.less+':eq('+((index*currentConfig.nbLess)+it)+')').click(function(){
+                    $(currentConfig.info+':eq('+index+')')[currentConfig.effect.up](currentConfig.time,function(){
+                        for (var k = 0; k < currentConfig.nbLess ; k++) {
+                            $(currentConfig.less+':eq('+((index*currentConfig.nbLess)+k)+')').hide();
+                        }
+                        for (var l = 0; l < currentConfig.nbMore; l++) {
+                            $(currentConfig.more+':eq('+((index*currentConfig.nbMore)+l)+')').show().css('cursor','pointer').focus();
+                        }
                     })
                 });
             }
          })
-
         Constants.GLOBAL.nbconf++;
         return $(this);
     };
 })(jQuery);
+
